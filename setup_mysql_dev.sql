bc7dd15fd1da1d40faf0cf9ecce579acf64e57ba
shaun-saisi <saisishaun4@gmail.com>
2024-04-17 08:46:54 +0000

    maneno na checker

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create a user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Callais7461!';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Select privileges on perfomance
